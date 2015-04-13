546bc19b5e2cb9660b2600aa64c8fa1258606747
Erica <chae_osu@yahoo.com>
2015-04-12 18:19:26 -0700

    edit script test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,15 @@
+var scrabble = function(word) {
 
-var word = function(input) {
-  var vowels = ['a', 'e', 'i', 'o', 'u'];
-  for (var i = 0; i <= vowels.length; i++) {
-    if(input === vowels[i])  {
-      return true;
+var pig_latin = "";
+
+var arrayLetters = word.split();
+
+var vowels = ['a','e','i', 'o', 'u'];
+
+    if ((word[0] === arrayLetters[0]) || (word[0] === arrayLetters[1]) || (word[0] === arrayLetters[2]) || (word[0] === arrayLetters[3]) || (word[0] === arrayLetters[4]))  {
+      pig_latin = word + "ay";
     }
-  }
 
+return pig_latin;
 
 };
-
-// var leapYear = function(year) {
-//   if ((year % 4 === 0) && (year % 100 !== 0)) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,11 @@
-describe('word', function() {
-  it('is true for words that start with a vowel', function() {
-    expect(word('a', 'e', 'i', 'o', 'u')).to.equal(true);
+describe('scrabble', function() {
+  it('for words that start with a vowel, add ay', function() {
+    expect(scrabble('a')).to.equal('aay');
   });
-
-  // it('add "ay" to the end', function() {
-  //   expect(word('a')).to.equal('aay');
-  // });
-
+  it('for words that start with e vowel, add ay', function() {
+    expect(scrabble('e')).to.equal('eay');
+  });
+  it('for words that start with i,o,u vowel, add ay', function() {
+    expect(scrabble('i')).to.equal('iay');
+  });
 });
